c31f3f075554f9cca5dccaf82144b393eb858710
Ezequiel Amin <ezequielamin@outlook.com>
2023-10-15 19:11:04 -0300

    feat: add dictionary, and finish first part of project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Lexer } from './models/Lexer.js';
 
-const sourceCode = 'int x = 42; y = x + 10;';
+const sourceCode = 'int x = 42; y = x + 10; float z = 5.2; if (x != 10) { y = 0; }';
 
 console.log(`Source code: ${sourceCode}\n`);
 
--- src/models/Lexer.js
+++ src/models/Lexer.js
@@ -1,74 +1,93 @@
+import { Analyzer } from './Analyzer.js';
 import { Token } from './Token.js';
+
+import dictionary from '../dictionary.js';
 
 export class Lexer {
   constructor(input) {
-    this.input = input;
+    // remove newlines
+    this.input = input.replace(/\n/g, ' ');
     this.position = 0;
-  }
-
-  isWhiteSpace(char) {
-    return /\s/.test(char);
-  }
-
-  isAlpha(char) {
-    return /[a-zA-Z_]/.test(char);
-  }
-
-  isDigit(char) {
-    return /\d/.test(char);
   }
 
   getNextToken() {
     while (this.position < this.input.length) {
-      let char = this.input[this.position];
+      let char = this.input.at(this.position);
 
-      if (this.isWhiteSpace(char)) {
+      if (Analyzer.isWhiteSpace(char)) {
         this.position += 1;
         continue;
       }
 
-      if (this.isAlpha(char)) {
+      // Identifiers and keywords
+      if (Analyzer.isAlpha(char)) {
         let identifier = '';
-        while (this.isAlpha(char) || this.isDigit(char)) {
+
+        while (Analyzer.isAlpha(char) || Analyzer.isDigit(char)) {
           identifier += char;
           this.position += 1;
-          char = this.input[this.position];
+          char = this.input.at(this.position);
         }
+
+        const keyword = dictionary.keywords[identifier];
+
+        if (keyword) {
+          return new Token(keyword, identifier);
+        }
+
         return new Token('IDENTIFIER', identifier);
       }
 
-      if (this.isDigit(char)) {
+      // Numbers
+      if (Analyzer.isDigit(char)) {
         let number = '';
-        while (this.isDigit(char)) {
+
+        while (Analyzer.isDigit(char)) {
           number += char;
           this.position += 1;
-          char = this.input[this.position];
+          char = this.input.at(this.position);
         }
-        return new Token('NUMBER', parseInt(number, 10));
+
+        // Floats
+        if (char === '.') {
+          number += char;
+          this.position += 1;
+          char = this.input.at(this.position);
+
+          // Check for ".(something_not_a_number)"
+          if (!Analyzer.isDigit(char)) {
+            throw new Error(`Invalid character: ${char}`);
+          }
+
+          while (Analyzer.isDigit(char)) {
+            number += char;
+            this.position += 1;
+            char = this.input.at(this.position);
+          }
+        }
+
+        return new Token('NUMBER', Number(number));
       }
 
-      switch (char) {
-        case '=':
-          this.position += 1;
-          return new Token('ASSIGN', '=');
-        case '+':
-          this.position += 1;
-          return new Token('PLUS', '+');
-        case '-':
-          this.position += 1;
-          return new Token('MINUS', '-');
-        case '*':
-          this.position += 1;
-          return new Token('MULTIPLY', '*');
-        case '/':
-          this.position += 1;
-          return new Token('DIVIDE', '/');
-        case ';':
-          this.position += 1;
-          return new Token('SEMICOLON', ';');
-        default:
-          throw new Error(`Invalid character: ${char}`);
+      // Symbols
+      const name = dictionary.symbols[char];
+      const value = char;
+
+      if (!name) {
+        throw new Error(`Invalid character: ${char}`);
       }
+
+      // Check for bigger symbols (>=, <=, ==, !=)
+      const nextChar = this.input.at(this.position + 1);
+      const biggerSymbol = dictionary.symbols[char + nextChar];
+
+      if (biggerSymbol) {
+        this.position += 2;
+        return new Token(biggerSymbol, char + nextChar);
+      }
+
+      this.position += 1;
+      return new Token(name, value);
     }
 
     return new Token('EOF', null);
